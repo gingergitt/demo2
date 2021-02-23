368be1af7121836571d5bf3f06e9f7be64a1f5e8
gingergitt <mro8822@gamil.com>
2021-02-23 10:00:01 +0900

    update

--- src/main/java/com/example/demo/AboutController.java
+++ src/main/java/com/example/demo/AboutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AboutController {
 	@RequestMapping("/abouts")
-	public String about() {
+	public String abouts() {
 		return "abouts";
 	}
 }
